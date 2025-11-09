6eb653f63426fcb4dc8cc64d0afa3c3a0bda3da8
Akash Kumar <akash29370@gmail.com>
2025-11-09 15:31:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your code here
+const boxes = document.querySelectorAll(".img-box");
+let draggedBox = null;
+
+boxes.forEach((box) => {
+  box.addEventListener("dragstart", () => {
+    draggedBox = box;
+  });
+
+  box.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  });
+
+  box.addEventListener("drop", () => {
+    if (draggedBox !== box) {
+    
+      let temp = box.style.backgroundImage;
+      box.style.backgroundImage = draggedBox.style.backgroundImage;
+      draggedBox.style.backgroundImage = temp;
+    }
+  });
+});
